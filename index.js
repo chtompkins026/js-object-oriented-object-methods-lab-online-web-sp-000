e03155c10ce229345f3ef493540be6c9f0ea4e8d
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-25 18:51:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function BoardMember(name, homeState, training){ 
    this.veto = function(){
-    console.log(`Hello everybody, my name is ${this.name}!`);
+    console.log(`No, I must disagree`);
   }
   
 }
